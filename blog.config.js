d723c733278e365d3e5e9845b416bd6f7a3079e4
HunterMocha <153085574+HunterMocha@users.noreply.github.com>
2023-12-09 14:22:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Henry Zhaozhang Zhu', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'An undergraduate philosophy student', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'www.zhaozhangzhu.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'www.zhaozhangzhu.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'About, Course, Research', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
